e689369712f6abe4d87dd2c9f9f0077d368dbbb7
Alex <thoms.alexander23@gmail.com>
2021-05-19 10:52:59 -0400

    Feat: updated gitignore and completed seeder

---
+++ albums_seeder.sql
@@ -0,0 +1,65 @@
+USE
+codeup_test_db;
+
+-- INSTRUCTORS GAVE FOR USE
+-- INSERT INTO albums (artist, record_name, release_date, sales, genre)
+-- VALUES ('Michael Jackson', 'Thriller', 1982, 'Pop, Rock, R&B', 47.3),
+--        ('AC/DC', 'Back in Black', 1980, 'Hard rock', 26.1),
+--        ('Pink Floyd', 'The Dark Side of the Moon', 1973, 'Progressive rock', 24.2),
+--        ('Meat Loaf', 'Bat Out of Hell', 1977, 'Hard rock, Progressive rock', 21.5),
+--        ('Whitney Houston / Various artists', 'The Bodyguard', 1992, 'R&B, Soul, Pop, Soundtrack', 28.4),
+--        ('Eagles', 'Their Greatest Hits (1971–1975)', 1976, 'Rock, Soft rock, Folk rock', 32.2),
+--        ('Bee Gees / Various artists', 'Saturday Night Fever', 1977, 'Disco', 21.6),
+--        ('Fleetwood Mac', 'Rumours', 1977, 'Soft rock', 27.9),
+--        ('Various artists', 'Grease: The Original Soundtrack from the Motion Picture', 1978, 'Soundtrack', 14.4),
+--        ('Led Zeppelin', 'Led Zeppelin IV', 1971, 'Hard rock, Heavy metal, Folk', 29.0),
+--        ('Michael Jackson', 'Bad', 1987, 'Pop, Funk, Rock', 19.3),
+--        ('Alanis Morissette', 'Jagged Little Pill', 1995, 'Alternative rock', 24.4),
+--        ('Shania Twain', 'Come On Over', 1997, 'Country, Pop', 29.6),
+--        ('Celine Dion', 'Falling into You', 1996, 'Pop, Soft rock', 20.2),
+--        ('The Beatles', 'Sgt. Pepper''s Lonely Hearts Club Band', 1967, 'Rock', 13.1),
+--        ('Eagles', 'Hotel California', 1976, 'Rock, Soft rock, Folk rock', 21.5),
+--        ('Various artists', 'Dirty Dancing', 1987, 'Pop, Rock, R&B', 17.9),
+--        ('Adele', '21', 2011, 'Pop, Soul', 25.3),
+--        ('Celine Dion', 'Let''s Talk About Love', 1997, 'Pop, Soft rock', 19.3),
+--        ('The Beatles', '1', 2000, 'Rock', 22.6),
+--        ('Michael Jackson', 'Dangerous', 1991, 'Rock, Funk, Pop', 16.3),
+--        ('Madonna', 'The Immaculate Collection', 1990, 'Pop, Dance', 19.4),
+--        ('The Beatles', 'Abbey Road', 1969, 'Rock', 14.4);
+
+
+-- USING SHEETS ('"&__&"')
+INSERT INTO albums (artist, record_name, release_date, sales, genre)
+VALUES ('Michael Jackson', 'Thriller', '1982', '66', 'Pop, post-disco, funk, rock')
+    ('AC/DC', 'Back in Black', '1980', '50', 'Hard rock')
+('Meat Loaf', 'Bat Out of Hell', '1977', '50', 'Hard rock, glam rock, progressive rock')
+('Pink Floyd', 'The Dark Side of the Moon', '1973', '45', 'Progressive rock')
+('Whitney Houston', 'The Bodyguard', '1992', '45', 'R&B, soul, pop, soundtrack')
+('Eagles', 'Their Greatest Hits (1971–1975)', '1976', '42', 'Country rock, soft rock, folk rock')
+('Bee Gees', 'Saturday Night Fever', '1977', '40', 'Disco')
+('Fleetwood Mac', 'Rumours', '1977', '40', 'Soft rock')
+('Shania Twain', 'Come On Over', '1997', '40', 'Country, pop')
+('Various artists', 'Grease: The Original Soundtrack from the Motion Picture', '1978', '38', 'Rock and roll')
+('Led Zeppelin', 'Led Zeppelin IV', '1971', '37', 'Hard rock, heavy metal, folk rock')
+('Michael Jackson', 'Bad', '1987', '35', 'Pop, rhythm and blues, funk and rock')
+('Alanis Morissette', 'Jagged Little Pill', '1995', '33', 'Alternative rock')
+('Michael Jackson', 'Dangerous', '1991', '32', 'New jack swing, R&B and pop')
+('Celine Dion', 'Falling into You', '1996', '32', 'Pop, soft rock')
+('Eagles', 'Hotel California', '1976', '32', 'Soft rock')
+('The Beatles', 'Sgt. Pepper''s Lonely Hearts Club Band', '1967', '32', 'Rock')
+('Various artists', 'Dirty Dancing', '1987', '32', 'Pop, rock, R&B')
+('Adele', '21', '2011', '31', 'Pop, soul')
+('Madonna', 'The Immaculate Collection', '1990', '31', 'Pop, dance')
+('Celine Dion', 'Let''s Talk About Love', '1997', '31', 'Pop, soft rock')
+('Metallica', 'Metallica', '1991', '31', 'Heavy metal')
+('The Beatles', '1', '2000', '31', 'Rock')
+('The Beatles', 'Abbey Road', '1969', '31', 'Rock')
+('ABBA', 'Gold: Greatest Hits', '1992', '30', 'Pop, disco')
+('Bruce Springsteen', 'Born in the U.S.A.', '1984', '30', 'Heartland rock')
+('Pink Floyd', 'The Wall', '1979', '30', 'Progressive rock')
+('James Horner', 'Titanic: Music from the Motion Picture', '1997', '30', 'Film score')
+('Dire Straits', 'Brothers in Arms', '1985', '30', 'Roots rock, blues rock, soft rock')
+('Nirvana', 'Nevermind', '1991', '30', 'Grunge, alternative rock')
+('Santana', 'Supernatural', '1999', '30', 'Latin rock')
+('Guns N'' Roses', 'Appetite for Destruction', '1987', '30', 'Hard rock')
+('Elton John', 'Goodbye Yellow Brick Road', '1973', '30', 'rock, pop rock, glam rock')
