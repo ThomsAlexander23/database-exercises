42500708af84a2d782eb32e5793bd031cd078e85
Alex <thoms.alexander23@gmail.com>
2021-05-21 10:51:25 -0400

    Feat: part II completed

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,7 +1,8 @@
 USE employees;
 
-SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya')\G
+-- SELECT * FROM employees WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR  first_name ='Maya') AND gender = 'm'\G
+-- SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya')\G
 
-SELECT * FROM employees WHERE last_name LIKE ('E%')\G
+-- SELECT * FROM employees WHERE last_name LIKE ('E%') AND last_name LIKE ('%E')\G
 
-SELECT * FROM employees WHERE last_name LIKE ('%q%')\G
+SELECT * FROM employees WHERE last_name LIKE ('%q%') AND last_name NOT LIKE ('%qu%')\G
